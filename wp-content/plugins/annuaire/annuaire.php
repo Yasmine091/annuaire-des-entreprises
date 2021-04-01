aae25335bb10e84b8bb25197e7edfac4d3be417c
Gitium <gitium@presslabs.com>
2021-04-01 16:35:36 +0200

    Design affichage annuaire

--- wp-content/plugins/annuaire/annuaire.php
+++ wp-content/plugins/annuaire/annuaire.php
@@ -8,22 +8,57 @@
 global $wpdb;
 $results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}annuaire", OBJECT);
 
+/* if(isset(filter)){
 
+} */
+
+function styles(){
+    wp_enqueue_style( 'ann_affichage', plugins_url('/css/affichage.css', __FILE__), false, '1.0.0', 'all');
+}
+add_action('wp_enqueue_scripts', "styles");
 
 function shortcode_annuaire($atts, $content = null) {
     ob_start();
     $results = (array) $GLOBALS['results'];
     ?>
-    
+    <nav id="ann-nav">
+        <form method="POST">
+
+            <label for="filtre">Rechercher :</label>
+
+            <div class="ann-container">
+            <input type="search" placeholder="Entreprise, localisation, nom, prénom, mail..">
+            </select>
+
+            <input type="submit" name="search" value="Rechercher">
+            </div>
+            
+        </form>
+
+        <form method="POST">
+
+            <label for="filtre">Filtrer par :</label>
+
+            <div class="ann-container">
+            <select name="filtre" id="filtre">
+            <option value="entreprise">Localisation</option>
+            <option value="entreprise">Entreprise</option>
+            </select>
+
+            <input type="submit" name="filter" value="Filtrer">
+            </div>
+            
+        </form>
+    </nav>
     <?php
     foreach ($results as $ann) {
 	?>
-    <ul>
-    <li><?php echo $ann->nom_entreprise; ?></li>
-    <li><?php echo $ann->localisation_entreprise; ?></li>
-    <li><?php echo $ann->prenom_contact; ?></li>
-    <li><?php echo $ann->nom_contact; ?></li>
-    <li><?php echo $ann->mail_contact; ?></li>
+    <ul class="ann-card">
+    <li><b>Entreprise: </b><?php echo $ann->nom_entreprise; ?></li>
+    <li><b>Localisation: </b><?php echo $ann->localisation_entreprise; ?></li>
+    <li><b>Prénom: </b><?php echo $ann->prenom_contact; ?></li>
+    <li><b>Nom: </b><?php echo $ann->nom_contact; ?></li>
+    <li><b>Mail: </b><?php echo $ann->mail_contact; ?></li>
     </ul>
     <?php
     }
